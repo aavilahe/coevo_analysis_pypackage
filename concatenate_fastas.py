accea8eb3970c0cc90052d0ebbb190f538a315a1
Aram Avila-Herrera <Aram.Avila-Herrera@ucsf.edu>
2015-03-19 12:01:21 -0700

    Uses SeqIO.to_dict() instead of creating mapping

--- concatenate_fastas.py
+++ concatenate_fastas.py
@@ -7,20 +7,18 @@
 '''
 
 import sys
-from Bio import AlignIO
+from Bio import AlignIO, SeqIO
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         sys.exit('usage: %s left.fa right.fa > left_right.fa' % sys.argv[0])
         
     left_aln = AlignIO.read(sys.argv[1], format = "fasta")
-    right_aln = AlignIO.read(sys.argv[2], format = "fasta")
-
-    right_map = dict(((seq.id, seqn) for seqn, seq in enumerate(right_aln)))
+    right_aln = SeqIO.to_dict(AlignIO.read(sys.argv[2], format = "fasta"))
 
     for left_seq in left_aln:
-        if left_seq.id in right_map:
-            print (left_seq + right_aln[right_map[left_seq.id]]).format('fasta'),
+        if left_seq.id in right_aln:
+            print (left_seq + right_aln[left_seq.id]).format('fasta'),
         else:
             print >>sys.stderr, "skipping: %s" % left_seq.id
 
