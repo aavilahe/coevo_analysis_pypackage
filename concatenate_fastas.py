40b0745431d9412d7464ecca18f391dea546b707
Aram Avila-Herrera <Aram.Avila-Herrera@ucsf.edu>
2015-03-18 18:52:43 -0700

    Makes concatenate_fastas.py depend on biopython

--- concatenate_fastas.py
+++ concatenate_fastas.py
@@ -1,53 +1,26 @@
 #!/usr/bin/env python
-''' concatenate_fastas.py -- concatenate corresponding sequences in two fastas
+''' concatenate_fastas.py -- concatenates two fastas horizontally
 
-	seqids must be exact and unique within each fasta
+    - Sequence identifiers must be exact and unique within each fasta
+    - Writes to stdout
 
 '''
 
 import sys
-import re
-
-def ReadFasta(fh):
-	''' Reads fasta file and returns alignment as a dict.
-	
-	'''
-
-	ali = dict()
-	header = ''
-	seq = ''
-
-	for line in fh:
-		line = line.rstrip('\n\r')
-		if line == '':
-			continue
-		if line.startswith('>'):
-			if header != '':
-				ali[header] =  re.sub('\s+', '',seq) 
-				seq = ''
-			header = line
-			continue
-		seq += line
-	ali[header] =  re.sub('\s+', '',seq) 
-	return ali
+from Bio import AlignIO
 
 if __name__ == "__main__":
-	if len(sys.argv) != 3:
-		sys.exit('usage: %s left.fa right.fa > left_right.fa'%sys.argv[0])
-	
-	left_ali = ReadFasta(open(sys.argv[1], 'r'))
-	right_ali = ReadFasta(open(sys.argv[2], 'r'))
-	
-	for seq_id, left_seq in left_ali.iteritems():
-		if seq_id in right_ali:
-			right_seq = right_ali[seq_id]
-			print seq_id
-			print left_seq
-			print right_seq
-		else:
-			print >>sys.stderr, 'skipping %s' % seq_id
+    if len(sys.argv) != 3:
+        sys.exit('usage: %s left.fa right.fa > left_right.fa' % sys.argv[0])
+        
+    left_aln = AlignIO.read(sys.argv[1], format = "fasta")
+    right_aln = AlignIO.read(sys.argv[2], format = "fasta")
 
+    right_map = dict(((seq.id, seqn) for seqn, seq in enumerate(right_aln)))
 
-	
-
+    for left_seq in left_aln:
+        if left_seq.id in right_map:
+            print (left_seq + right_aln[right_map[left_seq.id]]).format('fasta'),
+        else:
+            print >>sys.stderr, "skipping: %s" % left_seq.id
 
