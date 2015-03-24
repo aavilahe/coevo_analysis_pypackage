beae36118b49bc9506ccb80f27afd619532e2cbf
Aram Avila-Herrera <Aram.Avila-Herrera@ucsf.edu>
2015-03-23 18:54:07 -0700

    Finds subpackages automatically, bumps version

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name = 'coevo',
-        version = '0.1',
+        version = '0.2.0',
         description = 'library for comparing coevolution identifying methods',
         url = 'in progress',
         author = 'Aram Avila-Herrera',
         author_email = 'Aram.Avila-Herrera@ucsf.edu',
         license = 'GPL',
-        packages = [ 'coevo' ],
+        packages = find_packages(),
         install_requires = [ 'pandas', 'biopython' ],
         zip_safe = False
         )
