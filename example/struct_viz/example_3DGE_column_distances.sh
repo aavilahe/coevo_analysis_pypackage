b8c3ead7c7a92af45920c24b811836286bcb3963
Aram Avila-Herrera <Aram.Avila-Herrera@ucsf.edu>
2015-03-23 17:27:41 -0700

    Relocates coevo_struct_viz example; adds test data

--- example/struct_viz/example_3DGE_column_distances.sh
+++ example/struct_viz/example_3DGE_column_distances.sh
@@ -5,6 +5,8 @@
 #
 # 3DGE - A2B2 heterocomplex
 
+BIN="../../bin"
+
 PDB='3DGE.pdb.gz'
 
 CHAIN_L1='A'
@@ -20,17 +22,17 @@ MAPL1="${FA_L%.fa}-${CHAIN_L1}.col_res_aa"
 MAPR1="${FA_R%.fa}-${CHAIN_R1}.col_res_aa"
 MAPR2="${FA_R%.fa}-${CHAIN_R2}.col_res_aa"
 
-python map_column_to_resnum.py ${CHAIN_L1} ${PDB} ${FA_L} > ${MAPL1}
-python map_column_to_resnum.py ${CHAIN_R1} ${PDB} ${FA_R} > ${MAPR1}
-python map_column_to_resnum.py ${CHAIN_R2} ${PDB} ${FA_R} > ${MAPR2}
+python ${BIN}/map_column_to_resnum.py --ex_aln ${CHAIN_L1} ${PDB} ${FA_L} > ${MAPL1}
+python ${BIN}/map_column_to_resnum.py --ex_aln ${CHAIN_R1} ${PDB} ${FA_R} > ${MAPR1}
+python ${BIN}/map_column_to_resnum.py --ex_aln ${CHAIN_R2} ${PDB} ${FA_R} > ${MAPR2}
 
 # get distances and map
 DIST1="${PDB%.pdb*}.${CHAIN_L1}_${CHAIN_R1}.Cb.dist"
 DIST2="${PDB%.pdb*}.${CHAIN_L1}_${CHAIN_R2}.Cb.dist"
 MDIST="${PDB%.pdb*}.Cb.mindist"
 
-python get_dists.py --chainL=${CHAIN_L1} --chainR=${CHAIN_R1} --mapL=${MAPL1} --mapR=${MAPR1} --dtype=Cb ${PDB} > ${DIST1}
-python get_dists.py --chainL=${CHAIN_L1} --chainR=${CHAIN_R2} --mapL=${MAPL1} --mapR=${MAPR2} --dtype=Cb ${PDB} > ${DIST2}
+python ${BIN}/get_dists.py --chainL=${CHAIN_L1} --chainR=${CHAIN_R1} --mapL=${MAPL1} --mapR=${MAPR1} --dtype=Cb ${PDB} > ${DIST1}
+python ${BIN}/get_dists.py --chainL=${CHAIN_L1} --chainR=${CHAIN_R2} --mapL=${MAPL1} --mapR=${MAPR2} --dtype=Cb ${PDB} > ${DIST2}
 
-python min_dists.py ${DIST1} ${DIST2} > ${MDIST}
+python ${BIN}/min_dists.py ${DIST1} ${DIST2} > ${MDIST}
 
