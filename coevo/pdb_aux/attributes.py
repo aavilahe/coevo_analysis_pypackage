3a9d4f9bc958dae9bec86bece1ee6d08d2229837
Aram Avila-Herrera <Aram.Avila-Herrera@ucsf.edu>
2015-03-26 19:07:01 -0700

    Updates make_chimera_attributes.py formatting; optional chain_id

--- coevo/pdb_aux/attributes.py
+++ coevo/pdb_aux/attributes.py
@@ -10,7 +10,7 @@ def mangle_attr_name(attr_name):
 
     return 'a_' + attr_name
 
-def make_chimera_attributes(attr_df, chain_id):
+def make_chimera_attributes(attr_df, chain_id = ''):
     ''' Returns string of attribute control and assignment lines
 
         attr_df: pandas.Data.Frame in wide format, indexed by resnum
@@ -18,15 +18,23 @@ def make_chimera_attributes(attr_df, chain_id):
 
     '''
 
-    attr_str = ''
+    if chain_id is not '':
+        chain_spec = '.' + chain_id
+    else:
+        chain_spec = ''
 
+    attr_str = ''
     for attr_name in attr_df:
+        if attr_df[attr_name].dtype in (float, int):
+            attr_fmt = '\t%s\t%.6e\n'
+        else:
+            attr_fmt = '\t%s\t%s\n'
         attr_str += 'attribute: %s\n' % mangle_attr_name(attr_name)
         attr_str += 'match mode: 1-to-1\n'
         attr_str += 'recipient: residues\n'
         for (resn, attr_val) in attr_df[attr_name].iteritems():
-            atom_spec = ':%d.%s' % (resn, chain_id)
-            attr_str += '\t%s\t%s\n' % (atom_spec, attr_val)
+            atom_spec = ':%d%s' % (resn, chain_spec)
+            attr_str += attr_fmt % (atom_spec, attr_val)
 
     return attr_str
 
