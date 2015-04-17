956c1f8e03526dfdf5ade930ccf54c8e7bd30f34
Aram Avila-Herrera <Aram.Avila-Herrera@ucsf.edu>
2015-04-17 00:23:45 -0700

    Installs bin scripts

--- setup.py
+++ setup.py
@@ -8,6 +8,16 @@ setup(name = 'coevo',
         author_email = 'Aram.Avila-Herrera@ucsf.edu',
         license = 'GPL',
         packages = find_packages(),
-        install_requires = [ 'pandas', 'biopython' ],
+        install_requires = ['pandas', 'biopython'],
+        scripts = [
+                   'bin/fasta_to_phy.py',
+                   'bin/fasta_to_psicov.py',
+                   'bin/get_dists.py',
+                   'bin/join_fastas.py',
+                   'bin/make_attributes.py',
+                   'bin/map_column_to_resnum.py',
+                   'bin/min_dists.py',
+                   'bin/split_faa_on_col.py'
+                   ],
         zip_safe = False
         )
